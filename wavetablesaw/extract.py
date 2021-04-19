89537c51bc5773c1697872a6b80f04620ee87871
Straw Manninen <strawmanninen@outlook.com>
2021-04-19 12:05:40 +0300

    Added sub-command handling + stubs form commands

---
+++ wavetablesaw/extract.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2021 Straw Manninen <strawmanninen@outlook.com>
+#
+
+def extract_wavetable(file,
+                      outfile,
+                      insize,
+                      verbose=False):
+    pass
---
+++ wavetablesaw/shuffle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2021 Straw Manninen <strawmanninen@outlook.com>
+#
+
+def shuffle_wavetable(file,
+                      outfile,
+                      insize,
+                      verbose=False):
+    pass
